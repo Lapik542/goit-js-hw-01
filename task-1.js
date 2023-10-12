66dfd278f2f3d11be26bf2490fd4b06d3314f880
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-10-12 22:45:24 +0300

    Update task-1.js

---
+++ task-1.js
@@ -0,0 +1,6 @@
+const userName = "Alex";
+const userNikName = "Gavrish";
+let userAge = 30;
+const fromUser = "villege Rotmistrivka";
+userAge = 16;
+console.log(`I am ${ userName } ${ userNikName }. I am yours old ${userAge}. I from ${fromUser}.`)
