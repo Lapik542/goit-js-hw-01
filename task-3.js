4637e6a25c3ff4f701bf2819d87f798b2f23e5cf
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-10-26 16:26:26 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -8,21 +8,21 @@ console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
 
-// function isNumberInRange(start, end, number) {
-//    const a = start;
-//    const c = number;
-//    const b = end;
-//    if (c >= a && c <= b) {
-//       console.log(true);
-//    } else {
-//       console.log(false);
-//    }
-// }
+function isNumberInRange(start, end, number) {
+   const a = start;
+   const c = number;
+   const b = end;
+   if (c >= a && c <= b) {
+      console.log(true);
+   } else {
+      console.log(false);
+   }
+}
 
-// isNumberInRange(20, 50, 76)
+isNumberInRange(20, 50, 76)
 
-// isNumberInRange(20, 50, 24)
+isNumberInRange(20, 50, 24)
 
-// isNumberInRange(10, 30, 17)
+isNumberInRange(10, 30, 17)
 
-// isNumberInRange(10, 30, 5)
+isNumberInRange(10, 30, 5)
